5e10f8434125626b5f57d5071e0b0a42f54238a1
davidmthompson <davidthompson25@proton.me>
2026-02-24 09:17:54 -0700

    Initial Commit

---
+++ Dogs/DogsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogsApp.swift
+//  Dogs
+//
+//  Created by david thompson on 2/24/26.
+//
+
+import SwiftUI
+
+@main
+struct DogsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
